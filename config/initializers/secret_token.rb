c3ee09a0e9ab3bcb5ad5dcd65ef7e40163c9e4a2
Thia <thia@travelvast.com>
2014-03-09 16:12:14 -0700

    Initialize rep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VisaChecker::Application.config.secret_key_base = 'a612b8a78d706c315d06730f55e76bf836383cdf05b317adccc0bd3f73038ea9c632ef88b9fae3b5b63ec2ca4368191dfac9111e223fbd2106a4c784097091b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VisaChecker::Application.config.session_store :cookie_store, key: '_Visa_checker_session'
